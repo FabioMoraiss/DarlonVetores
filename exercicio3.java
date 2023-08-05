00596a13afabd6e8deb6a6c0e7508a367b46ceb6
FabioMoraiss <rarinaldo@outlook.com>
2023-08-05 10:18:53 -0300

    primeiro commit

---
+++ exercicio3.java
@@ -0,0 +1,39 @@
+public class exercicio3 {
+    public static void main(String[] args) {
+        int[] bolinhoDeArroz = new int[10];
+
+        //prencher o arry com valors de -100 até 0;
+        bolinhoDeArroz = array.preencherArrayInteiro(bolinhoDeArroz, -100, 100);
+
+        //exibe o vetor da primra vez
+        array.exibirVetor(bolinhoDeArroz); 
+
+        // pula linha
+        System.out.println(); 
+
+        // faz a lógica do exerciccio
+        bolinhoDeArroz = regradoExercicio(bolinhoDeArroz); 
+
+        // exibe o vetor que o exercicio pede
+        array.exibirVetor(bolinhoDeArroz); 
+
+
+    }
+
+    public static int[] regradoExercicio(int[] bolinho) {
+
+         // criei um novo array como tamanho do array passado por parametro
+        int[] arroz = new int[bolinho.length];
+
+        for(int i =0; i<arroz.length; i++) { //for pra percorrer o vetor arroz
+            
+            if(bolinho[i] < 0) { //condição do exerciccio
+                arroz[i] = 0;
+            } else { 
+                arroz[i] = bolinho [i];
+            }
+        }
+
+        return arroz;
+    }
+}
---
+++ exercicio4.java
@@ -0,0 +1,62 @@
+public class exercicio4 {
+    public static void main(String[] args) {
+
+        int[] banana = new int[6];
+
+        banana = array.preencherArrayInteiro(banana, 6, 1);
+
+        array.exibirVetor(banana);
+        System.out.println(); // pular linha 
+
+        //regras do exercicio
+        int maiorNumeroLido = maiorNumeroLido(banana);
+        int menorNumeroLido = menorNumeroLido(banana);
+        int iguaisAoPrimeiro = igualAoPrimeiro(banana);
+
+        System.out.println("o maior numero lido foi: " + maiorNumeroLido);
+        System.out.println("o menor numeor lido foi: " + menorNumeroLido);
+        System.out.println("quantidade de numeros iguais ao primeiro: " + iguaisAoPrimeiro);
+
+    }
+
+    public static int maiorNumeroLido(int[] maca)  {
+        
+        // pega o MENOR numero inteiro possível em java, que é -2 147 483 648
+        int maior = Integer.MIN_VALUE;
+
+        for(int i = 0; i<maca.length; i++) {
+            if(maca[i] > maior) {
+                maior = maca[i];
+            }
+        }
+
+        return maior;
+    }
+
+    public static int menorNumeroLido(int[] pera) {
+
+        // pega o MAIOR numero inteiro possível em java, que é 2 147 483 648
+        int menor = Integer.MAX_VALUE;
+
+        for(int i = 0; i<pera.length; i++) {
+            if(pera[i] < menor) {
+                menor = pera[i];
+            }
+        }
+
+        return menor;
+    }
+
+    public static int igualAoPrimeiro(int[] manga) {
+
+        int quantidade=0; // contador
+
+        for(int i =1; i<manga.length; i++) { //começa do 1 porque 0(primeiro numero) NAO CONTA  
+
+            if(manga[i] == manga[0]) {
+                quantidade ++;
+            }
+        }
+        return quantidade;
+    }
+}
